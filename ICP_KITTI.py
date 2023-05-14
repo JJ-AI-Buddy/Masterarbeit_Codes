a6c6bd011b842ee317aac2c35a1ac5405e403376
JJ-AI-Buddy <116299770+JJ-AI-Buddy@users.noreply.github.com>
2023-05-13 21:45:10 -0400

    Added functionality in ICP_Kitti.py and added ToDOs.txt

--- ICP_KITTI.py
+++ ICP_KITTI.py
@@ -8,10 +8,11 @@ import open3d as o3d
 import copy
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 from datetime import datetime
 from SupportFunctions import *
 
-#### Function for getting cropped pc (with rectangular bbox)
+#### Functions
 def crop_pc_bbox (pc, min_bd,max_bd):
     min_bound=min_bd
     max_bound=max_bd
@@ -24,13 +25,94 @@ def downsampling_pc (pc, voxel_size):
     pc_down = pc.voxel_down_sample(voxel_size)
     
     return pc_down
+
+def detect_ground_thres(pc, thres_z, thres_norm_min, thres_norm_max):
     
+# Estimate normals for each point (big radius because the input pc is already downsampled)
+
+    pc.estimate_normals(
+        search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=2, max_nn=30))
+    
+    #GREEN = [0., 1., 0.]
+
+    # Get the min value along the z-axis:
+    z_min = min(pc.points, key=lambda x: x[2])[2]
 
 
+    # Get the original points color to be updated:
+    #pc.paint_uniform_color([0, 0.706, 1])
+    #pc_colors = np.asarray(pc.colors)
+
+
+    # Number of points:
+    #n_points = pc_colors.shape[0]
+
+
+    # update color:
+    #for i in range(n_points):
+        # if the current point is a ground point:
+    #    if pc.points[i][2] <= z_min + threshold:
+            #pc_colors[i] = GREEN  # color it green
+
+    #pc.colors = o3d.utility.Vector3dVector(pc_colors)
+    
+    # Only select non-ground points for further operations
+    ## z-value is greater than a specific threshold AND
+    ## the normal is not parallel to the z-axis of the KS, (-0.9 , 0.9)
+    
+    pc = pc.select_by_index(np.where((np.asarray(pc.points)[:, 2] > z_min + thres_z) &
+                                     (np.asarray(pc.normals)[:,2] < thres_norm_max) &
+                                     (np.asarray(pc.normals)[:,2] > thres_norm_min))[0])
+
+    return pc
+
+def DBSCAN_clustering(pc,eps,min_points):
+    
+    with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug):
+        labels = np.array(pc.cluster_dbscan(eps=eps, min_points=min_points, print_progress=False))
+
+    max_label = labels.max()
+    print(f"point cloud has {max_label + 1} clusters")
+
+    #colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+    #colors[labels < 0] = 0
+    #pc_without_ground.colors = o3d.utility.Vector3dVector(colors[:, :3])
+    
+    np_clust_centers = np.zeros((max_label+1,3))
+    clust_bboxes = []
+    
+    np_pc = np.asarray(pc.points)
+    np_pc_clust = np.insert(np_pc,0,labels,1)
+    
+    for lb in range(0,max_label+1):
+        np_obj = np_pc_clust[np_pc_clust[:,0] == lb]
+        # center x
+        np_clust_centers [lb][0] = np_obj[:,1].mean()
+        # center y
+        np_clust_centers [lb][1] = np_obj[:,2].mean()
+        # center z
+        np_clust_centers [lb][2] = np_obj[:,3].mean()
+        
+        obj = o3d.geometry.PointCloud()
+        obj.points = o3d.utility.Vector3dVector(np_obj[:,1::])
+        obj_bbox = obj.get_axis_aligned_bounding_box()
+        obj_bbox.color=(0,0,0)
+        clust_bboxes.append(obj_bbox)
+        
+    pc_clusters = o3d.geometry.PointCloud()
+    pc_clusters.points = o3d.utility.Vector3dVector(np_clust_centers)
+    pc_clusters.paint_uniform_color([0, 0, 1.0])
+    
+    # return point cloud containing all the centroids of the clusters in blue
+    # return list of bboxes of all clusters in black
+    return pc_clusters, clust_bboxes
+
+    
+############### SETTINGS ###################################################################################
 timestamp_src = 1
 timestamp_trg = 1
 
-name_txt = "TEST_05102023.txt"
+name_txt = "TEST_05132023.txt"
 
 
 path_to_data = r"C:\Users\Johanna\Downloads\2011_09_26\2011_09_26_drive_0001_sync\velodyne_points\data"
@@ -40,8 +122,9 @@ path_src = os.path.join(path_to_data, str(timestamp_src).zfill(10) + ".bin")
 path_trg  = os.path.join(path_to_data, str(timestamp_trg).zfill(10) + ".bin")
 
 path_txt = os.path.join(path_to_file, name_txt)
+####################################################################################################################
 
-
+# Load point clouds as PCD object
 pc_1 = KITTI_Bin2PCD(path_src)
 pc_2 = KITTI_Bin2PCD(path_trg)
 
@@ -49,27 +132,86 @@ source_pc = copy.deepcopy(pc_1)
 target_pc = copy.deepcopy(pc_2)
 
 
-##### DOWNSAMPLING
-
+# Voxel based downsampling of the PCs
 source_pc = downsampling_pc(source_pc,0.5)
 target_pc = downsampling_pc(target_pc,0.5)
 
+# Statistical outlier removal (alternative: Radial outlier removal)
+cl_src, ind_src = source_pc.remove_statistical_outlier(nb_neighbors=10,std_ratio=2.5)
+cl_trg, ind_trg = target_pc.remove_statistical_outlier(10,2.5)
 
-### CROPPING
+source_pc = cl_src.select_by_index(ind_src)
+target_pc = cl_trg.select_by_index(ind_trg)
+
+# Remove ground points from both PCs
+source_pc = detect_ground_thres(source_pc, 2.0, -0.95,0.95)
+target_pc = detect_ground_thres (target_pc, 2.0, -0.95, 0.95)
+
+# Cropping of the target pc with a bounding box which represents the overlapping area of both PCs
 
 target_pc = crop_pc_bbox(target_pc, (-50,-50,-50),(-10,50,50))
 
+# Initial pose guess: Initial transform matrix
 transform_matrix = np.asarray([[1.0, 0.0, 0.0, 5], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
-#source_pc = source_pc.transform(transform_matrix)
+source_pc = source_pc.transform(transform_matrix)
 
-#source_pc.paint_uniform_color([1, 0.706, 0])
-#target_pc.paint_uniform_color([0, 0.651, 0.929])
-#o3d.visualization.draw_geometries([source_pc, target_pc],
- #                                 zoom=0.4459, front=[0.9288, -0.2951, -0.2242],
-#                                  lookat=[1.6784, 2.0612, 1.4451],
-#                                  up=[-0.3402, -0.9189, -0.1996])
+# Paint the PCs in distinctive colors
+source_pc.paint_uniform_color([1, 0.0, 0.5])
+target_pc.paint_uniform_color([0, 1, 0.5])
+
+# Create KS for visualization
+origin = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.5)
+
+# Clustering
+pc_clust, bboxes_3D = DBSCAN_clustering(source_pc,5,10)
+
+# Project points to xy-plane
+np_clust_xy = np.asarray(source_pc.points)
+np_clust_xy = np.around(np_clust_xy,2)
+np_clust_xy[:,2] = 0 
+np_clust_xy =  np.unique(np_clust_xy, axis=0)
+
+pc_clust_xy = o3d.geometry.PointCloud()
+pc_clust_xy.points = o3d.utility.Vector3dVector(np_clust_xy)
+
+pc_clust_xy, bboxes_2D = DBSCAN_clustering(pc_clust_xy,5,10)
+pc_clust_xy.paint_uniform_color([1, 0, 0])
 
 
+##### Create graph with points from clustererd pc
+lineset = o3d.geometry.LineSet()
+
+# Sort point clouds by x-value from lowest to highest
+arr = np.asarray(pc_clust.points)
+arr = arr[arr[:, 0].argsort()]
+
+lineset.points = o3d.utility.Vector3dVector(arr)
+
+# Matrix with indices also must be created automatically!!!!
+lineset.lines = o3d.utility.Vector2iVector([[0, 1],
+                                           [1, 2],
+                                           [2, 3],
+                                           [3, 4],
+                                           [4,5],
+                                           [5,6],
+                                           [6,7],
+                                           [7,8]])
+
+
+
+visualizer_list = bboxes_3D
+visualizer_list.append(origin)
+visualizer_list.append(source_pc)
+visualizer_list.append(pc_clust)
+visualizer_list.append(lineset)
+
+o3d.visualization.draw_geometries(visualizer_list,
+                                 zoom=0.7, front=[0, 0, 1],
+                                 lookat=[0, 0, 0],
+                                 up=[0, 1, 0],
+                                 point_show_normal=False)
+
+################### MAIN #######################################
 
 with open(path_txt, 'w') as f:
     f.write('Evaluation of ICP algorithm for pc scan matching on KITTI dataset "2011_09_26_drive_0001_sync"\n ' + str(datetime.now()) + "\n\n")
--- SupportFunctions.py
+++ SupportFunctions.py
@@ -37,13 +37,15 @@ def draw_registration_result(source, target, transformation):
     """
     source_temp = copy.deepcopy(source)
     target_temp = copy.deepcopy(target)
-    source_temp.paint_uniform_color([1, 0.706, 0])
-    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.paint_uniform_color([1, 0.0, 0.5])
+    target_temp.paint_uniform_color([0, 1, 0.5])
     source_temp.transform(transformation)
-    o3d.visualization.draw_geometries([source_temp, target_temp],
-                                      zoom=0.4459, front=[0.9288, -0.2951, -0.2242],
-                                      lookat=[1.6784, 2.0612, 1.4451],
-                                      up=[-0.3402, -0.9189, -0.1996])
+    origin = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.5)
+    o3d.visualization.draw_geometries([source_temp, target_temp, origin],
+                                      zoom=0.7, front=[0, 0, 1],
+                                      lookat=[0, 0, 0],
+                                      up=[0, 1, 0],
+                                      point_show_normal=False)
     
 
 def find_nearest_neighbors(source_pc, target_pc, nearest_neigh_num):
